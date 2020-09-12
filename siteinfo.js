d730c20d75c08ef7712f48e2202c0f16893d01eb
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-09-12 15:29:09 +0800

    tiger 9-12

---
+++ siteinfo.js
@@ -0,0 +1,4 @@
+var siteinfo = { "uniacid": "31", "acid": "31", "multiid": "0", "version": "df", "siteroot":"https://api.site100.cn/app/index.php", "design_method":"3"}
+
+
+module.exports = siteinfo;
