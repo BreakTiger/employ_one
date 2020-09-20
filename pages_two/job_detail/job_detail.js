1a85f3628c541d1b13b220b979e698c7c88baafa
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-09-20 09:45:02 +0800

    tiger 9-20

--- pages_two/job_detail/job_detail.js
+++ pages_two/job_detail/job_detail.js
@@ -5,13 +5,58 @@ import modal from '../../modals.js'
 Page({
 
   data: {
-    id: ''
+    id: '',
+
+    position: {},
+
+    enterprise: {}
   },
 
   onLoad: function (options) {
     console.log(options.id)
     this.setData({
       id: options.id
+    })
+    this.getBase(options.id)
+
+    this.getEnterprise(options.id)
+
+
+  },
+
+  // 基本数据
+  getBase: function (id) {
+    let that = this
+    let data = {
+      id: id
+    }
+    util.sendRequest('/jeecg-boot/hall/position/list', 'get', data).then(function (res) {
+      console.log(res)
+      if (res.code == 0) {
+        that.setData({
+          position: res.result.records[0]
+        })
+      } else {
+        modal.showToast(res.messgae, 'none')
+      }
+    })
+  },
+
+  // 企业数据
+  getEnterprise: function (id) {
+    let that = this
+    let data = {
+      id: id
+    }
+    util.sendRequest('/jeecg-boot/hall/enterprise/list', 'get', data).then(function (res) {
+      console.log(res)
+      if (res.code == 0) {
+        that.setData({
+          enterprise: res.result.records[0]
+        })
+      } else {
+        modal.showToast(res.message, 'none')
+      }
     })
   },
 
