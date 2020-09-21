a09272faacefc73e37f5ef6724c2667edb936fce
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-09-21 18:05:24 +0800

    tiger 9-21

--- pages/workerdetail/workerdetail.js
+++ pages/workerdetail/workerdetail.js
@@ -2,18 +2,32 @@ const app = getApp()
 const util = require('../../utils/util.js')
 import modal from '../../modals.js'
 
+const WxParse = require('../../wxParse/wxParse.js')
+
 
 Page({
 
   data: {
-    detail: {}
+    detail: {},
+    header:''
   },
 
   onLoad: function (options) {
+    let that = this;
     console.log(app.globalData.worker)
-    this.setData({
+    that.setData({
       detail: app.globalData.worker
     })
+
+    if(app.globalData.worker.headimgAddress){
+      that.setData({
+        header:app.globalData.imaUrl + app.globalData.worker.headimgAddress
+      })
+    }
+
+    // let article = app.globalData.worker.jobDescription
+    // WxParse.wxParse('article', 'html', article, that, 5)
+
   },
 
 
