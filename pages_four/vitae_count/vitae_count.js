10bd60433e11400fc5adffe475d47b558e6d53f6
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-09-24 14:41:45 +0800

    tiger 9-24

--- pages_four/vitae_count/vitae_count.js
+++ pages_four/vitae_count/vitae_count.js
@@ -1,66 +1,88 @@
-// pages_four/vitae_count/vitae_count.js
+const app = getApp()
+const util = require('../../utils/util.js')
+import modal from '../../modals.js'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    page: 1,
+    list: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 列表
+  getList: function () {
+    let that = this
+    let data = {
+      enterpriseInfoId: wx.getStorageSync('company').id,
+      pageNo: that.data.page,
+      pageSize: 10
+    }
+    util.sendRequest('/jeecg-boot/app/interview/receivelist', 'get', data).then(function (res) {
+      console.log(res.result.records)
+      if (res.code == 0) {
+        that.setData({
+          list: res.result.records
+        })
+      } else {
+        modal.showToast(res.messgae, 'none')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 查看简历
+  toWatch: function (e) {
+    let that = this
+    let data = {
+      id: e.currentTarget.dataset.id
+    }
+    util.sendRequest('/jeecg-boot/hall/curriculumvitae/list', 'get', data).then(function (res) {
+      if (res.code == 0) {
+        let item = res.result.records[0]
+        let age = util.ages(item)
+        item.age = age
+        app.globalData.worker = item
+        wx.navigateTo({
+          url: '/pages/workerdetail/workerdetail',
+        })
+      } else {
+        modal.showToast(res.message)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 邀请面试
+  toInvite: function (e) {
+    let that = this
+    let detail = e.currentTarget.dataset.item
+    console.log(detail)
+    wx.showModal({
+      title: '提示',
+      content: "是否发送面试邀约给该投递人",
+      success: function (res) {
+        if (res.confirm) {
+          let data = {
+            createBy: wx.getStorageSync('company').id,
+            curriculumVitaeId: detail.curriculumVitaeId,
+            enterpriseInfoId: detail.enterpriseInfoId,
+            enterprisePostReleaseId: detail.enterprisePostReleaseId,
+            interviewstate: 'invite'
+          }
+          util.sendRequest('/jeecg-boot/app/interview/invite', 'post', data).then(function (res) {
+            console.log(res)
+            if (res.code == 200) {
+              modal.showToast(res.message)
+            } else {
+              modal.showToast(res.messgae, 'none')
+            }
+          })
+        }
+      }
+    })
   }
+
+
 })
