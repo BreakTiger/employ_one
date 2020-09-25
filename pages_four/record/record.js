4632e752d401f4f1c99224674435d2d4e976eae2
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-09-25 10:33:39 +0800

    tiger 9-25

--- pages_four/record/record.js
+++ pages_four/record/record.js
@@ -68,5 +68,35 @@ Page({
     wx.navigateTo({
       url: '/pages_four/add_record/add_record',
     })
-  }
+  },
+
+  // 删除记录
+  toDetele: function (e) {
+    let that = this
+    wx.showModal({
+      title: "提示",
+      content: "是否删除该面试记录",
+      success: function (res) {
+        if (res.confirm) {
+          let index = e.currentTarget.dataset.index
+          let list = that.data.list
+          let data = {
+            id: e.currentTarget.dataset.id
+          }
+          util.sendRequest('/jeecg-boot/app/interview/delete', 'get', data).then(function (res) {
+            console.log(res)
+            if (res.code == 200) {
+              modal.showToast(res.message)
+              list.splice(index, 1)
+              that.setData({
+                list: list
+              })
+            } else {
+              modal.showToast(res.message, 'none')
+            }
+          })
+        }
+      }
+    })
+  },
 })
