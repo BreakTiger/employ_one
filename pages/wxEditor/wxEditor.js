a7a25094f032644915c456e9ad9abe55578cadd4
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-10-28 16:44:11 +0800

    tiger 10-28

--- pages/wxEditor/wxEditor.js
+++ pages/wxEditor/wxEditor.js
@@ -1,66 +1,133 @@
-// pages/wxEditor/wxEditor.js
+const app = getApp()
+const util = require('../../utils/util.js')
+import modal from '../../modals.js'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
-
+    html: '',
+    formats: {},
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let html = app.globalData.describe
+    if (html) {
+      this.setData({
+        html: html
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 初始化
+  onEditorReady: function () {
+    let that = this
+    wx.createSelectorQuery().select('#editor').context(function (res) {
+      that.editorCtx = res.context;
+      that.editorCtx.setContents({
+        html: that.data.html
+      })
+    }).exec()
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  updatePosition(keyboardHeight) {
+    const toolbarHeight = 50
+    const { windowHeight, platform } = wx.getSystemInfoSync()
+    let editorHeight = keyboardHeight > 0 ? (windowHeight - keyboardHeight - toolbarHeight) : windowHeight
+    this.setData({ editorHeight, keyboardHeight })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  calNavigationBarAndStatusBar() {
+    const systemInfo = wx.getSystemInfoSync()
+    const { statusBarHeight, platform } = systemInfo
+    const isIOS = platform === 'ios'
+    const navigationBarHeight = isIOS ? 44 : 48
+    return statusBarHeight + navigationBarHeight
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  blur() {
+    this.editorCtx.blur()
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  format(e) {
+    let { name, value } = e.target.dataset
+    if (!name) return
+    this.editorCtx.format(name, value)
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onStatusChange(e) {
+    const formats = e.detail
+    this.setData({ formats })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  insertDivider() {
+    this.editorCtx.insertDivider({
+      success: function () {
+        console.log('insert divider success')
+      }
+    })
+  },
 
-  }
+  clear() {
+    this.editorCtx.clear({
+      success: function (res) {
+        console.log("clear success")
+      }
+    })
+  },
+
+  removeFormat() {
+    this.editorCtx.removeFormat()
+  },
+
+  insertDate() {
+    const date = new Date()
+    const formatDate = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+    this.editorCtx.insertText({
+      text: formatDate
+    })
+  },
+
+  // 插入图片
+  insertImage() {
+    const that = this
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        that.editorCtx.insertImage({
+          src: res.tempFilePaths[0],
+          data: {
+            id: 'abcd',
+            role: 'god'
+          },
+          width: '80%',
+          success: function () {
+            console.log('insert image success')
+          }
+        })
+      }
+    })
+  },
+
+  // 保存
+  toSave: function () {
+    let that = this
+    that.editorCtx.getContents({
+      success: function (res) {
+        console.log(res.html)
+        let html = res.html
+        let pages = getCurrentPages();
+        let prevPage = pages[pages.length - 2]; //上一个页面
+        prevPage.setData({
+          describe: html
+        })
+        wx.navigateBack({//返回
+          delta: 1
+        })
+      }
+    })
+  },
+
+
+
 })
